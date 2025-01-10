366d443cf288edb18e219c8666c77afe55bc46d3
tkhananiev <timur712@gmail.com>
2025-01-10 23:30:58 +0300

    Fix

--- src/main/java/mephi/java/exam/service/LinkService.java
+++ src/main/java/mephi/java/exam/service/LinkService.java
@@ -18,7 +18,6 @@ public class LinkService {
     public Link createLink(User user, String originalUrl, Integer requestLimit){
         String shortUrl = BASE_URL+generateShortCode();
         int limit = (requestLimit == null || requestLimit <= 0) ? ConfigManager.getClicksLimit(): requestLimit;
-        //Link Creation String originalUrl, String shortUrl, String ownerUuid, long lifeTimeSeconds, long maxClicks
         Link link = new Link(originalUrl, shortUrl, user.getUuid(), ConfigManager.getLinkLifeTimeSeconds(), limit);
         linkMap.put(shortUrl, link);
         user.addLink(link);
